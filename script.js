0686ed39d815d717aedf11906c159cb5ae8c9045
saad <meddahsaad14@gmail.com>
2023-10-21 22:13:23 +0100

    ai tesing

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+function submitForm() {
+    const form = document.getElementById('question-form');
+    const formData = new FormData(form);
+    const data = {};
+
+    formData.forEach((value, key) => {
+        if (!data[key]) {
+            data[key] = [value];
+        } else {
+            data[key].push(value);
+        }
+    });
+
+    console.log(data);
+    // You can send this data to your server for processing or perform any desired actions.
+}
